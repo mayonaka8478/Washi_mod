d250725cac1a35f8645768a870466f155ec22fdf
mayonaka8478 <mayonaka8478comb@gmail.com>
2024-04-06 18:43:42 +0900

    おくら

--- src/main/java/turniplabs/Bamboo-Remake/Bamboo_Remake.java
+++ src/main/java/turniplabs/Bamboo-Remake/Bamboo_Remake.java
@@ -3,12 +3,11 @@ package turniplabs.examplemod;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import turniplabs.halplibe.helper.BlockBuilder;
 import turniplabs.halplibe.util.GameStartEntrypoint;
 import turniplabs.halplibe.util.RecipeEntrypoint;
 
 
-public class Bamboo-Remake implements ModInitializer, GameStartEntrypoint, RecipeEntrypoint {
+public class Bamboo_Remake implements ModInitializer, GameStartEntrypoint, RecipeEntrypoint {
     public static final String MOD_ID = "examplemod";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
     @Override
