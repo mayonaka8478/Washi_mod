e8b22ec2d9d87a943a6344518272907cdb50239f
baguchan <shutux500@gmail.com>
2024-06-03 23:06:00 +0900

    material test

--- src/main/java/mayonaka8478/bambooremake/mixin/ItemHoeMixin.java
+++
@@ -1,9 +0,0 @@
-package mayonaka8478.bambooremake.mixin;
-
-import net.minecraft.core.item.tool.ItemToolHoe;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(ItemToolHoe.class)
-public class ItemHoeMixin {
-
-}
