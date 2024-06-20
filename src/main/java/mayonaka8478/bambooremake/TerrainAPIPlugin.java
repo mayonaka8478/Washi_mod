3e834e79a3626ee537193c4977cc4af19bdebd7b
mayonaka8478 <mayonaka8478comb@gmail.com>
2024-06-20 19:29:41 +0900

    筍の自然生成を追加しようとしたけどうまく動かなかった らこ

---
+++ src/main/java/mayonaka8478/bambooremake/TerrainAPIPlugin.java
@@ -0,0 +1,21 @@
+package mayonaka8478.bambooremake;
+
+import mayonaka8478.bambooremake.block.ModBlocks;
+import net.minecraft.core.world.biome.Biome;
+import net.minecraft.core.world.biome.Biomes;
+import net.minecraft.core.world.generate.feature.WorldFeatureFlowers;
+import useless.terrainapi.api.TerrainAPI;
+import useless.terrainapi.generation.overworld.api.ChunkDecoratorOverworldAPI;
+
+public class TerrainAPIPlugin implements TerrainAPI {
+	@Override
+	public String getModID() {
+		return BambooRemake.MOD_ID;
+	}
+
+	@Override
+	public void onInitialize() {
+		ChunkDecoratorOverworldAPI.randomFeatures.addFeature(new WorldFeatureFlowers(ModBlocks.bamboo_shoot.id), 5, -1f, 1,
+			new Biome[]{Biomes.OVERWORLD_RETRO, Biomes.OVERWORLD_FOREST, Biomes.OVERWORLD_RAINFOREST, Biomes.OVERWORLD_BIRCH_FOREST, Biomes.OVERWORLD_SEASONAL_FOREST, Biomes.OVERWORLD_SHRUBLAND, Biomes.OVERWORLD_BOREAL_FOREST, Biomes.OVERWORLD_TAIGA});
+	}
+}
